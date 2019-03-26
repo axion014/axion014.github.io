6260eac81a40b70b8f2b171f683a078930d11966
axion014 <axion014@gmail.com>
2019-03-26 23:37:51 +0900

    card hunter items/cards: add matching to cards in items

--- content/card-hunter-items-cards/main.js
+++ content/card-hunter-items-cards/main.js
@@ -19,11 +19,23 @@ function applySearch() {
 			return a + b + ".* ";
 		}, '^').trim(), 'i');
 	} catch(e) {} // 正規表現エラーは握りつぶす
+	var resultcache = {name: {}, acronym: {}};
 	for (var i = 0; i < list.children.length; i++) {
   	list.children[i].hidden = !(function() {
 			var name = list.children[i].getElementsByTagName('span')[0].innerText;
 			if (regex.test(name)) return true;
-			return regexAcronym.test(name);
+			if (regexAcronym.test(name)) return true;
+			if (list.children[i].data.cards) for (var j = 0; j < list.children[i].data.cards.length; j++) {
+				var card = list.children[i].data.cards[j];
+				// = operator intended
+				if (resultcache.name[card] !== undefined) {
+					if (resultcache.name[card]) return true;
+				} else if (resultcache.name[card] = regex.test(card)) return true;
+				if (resultcache.acronym[card] !== undefined) {
+					if (resultcache.acronym[card]) return true;
+				} else if (resultcache.acronym[card] = regexAcronym.test(card)) return true;
+			}
+			return false;
 		})();
 	}
 }
@@ -32,6 +44,7 @@ db.collection("data").doc("data").get().then(function(data) {
 	data = data.data();
 	data.cards.forEach(function(card) {
 		var element = document.createElement('li');
+		element.data = card;
 		//element.id = card.name;
 		var image = document.createElement('img');
 		image.className = "lazy";
@@ -50,6 +63,7 @@ db.collection("data").doc("data").get().then(function(data) {
 	});
 	data.items.forEach(function(item) {
 		var element = document.createElement('li');
+		element.data = item;
 		//element.id = item.name;
 		if (item.image_url) {
 			var image = document.createElement('img');
